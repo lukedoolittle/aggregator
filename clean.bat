5fbfc7d7d4afc8d0eb6a4045e50e07b5d0939d5b
lukedoolittle <LDoolittle@musicnotes.com>
2016-08-19 14:26:53 -0500

    Testing for v0.2.0 release

--- clean.bat
+++ clean.bat
@@ -7,6 +7,25 @@ rmdir /s /q src\Material.Windows\obj
 rmdir /s /q src\Material\bin
 rmdir /s /q src\Material\obj
 
+rmdir /s /q src\Foundations\bin
+rmdir /s /q src\Foundations\obj
+rmdir /s /q src\Foundations.Bootstrap\bin
+rmdir /s /q src\Foundations.Bootstrap\obj
+rmdir /s /q src\Foundations.Cryptography\bin
+rmdir /s /q src\Foundations.Cryptography\obj
+rmdir /s /q src\Foundations.Http.Android\bin
+rmdir /s /q src\Foundations.Http.Android\obj
+rmdir /s /q src\Foundations.Http.iOS\bin
+rmdir /s /q src\Foundations.Http.iOS\obj
+rmdir /s /q src\Foundations.Http.Windows\bin
+rmdir /s /q src\Foundations.Http.Windows\obj
+rmdir /s /q src\Foundations.HttpClient\bin
+rmdir /s /q src\Foundations.HttpClient\obj
+rmdir /s /q src\Foundations.Serialization\bin
+rmdir /s /q src\Foundations.Serialization\obj
+
+rmdir /s /q test\Foundations.Test\bin
+rmdir /s /q test\Foundations.Test\obj
 rmdir /s /q test\Quantfabric.Test.Android\bin
 rmdir /s /q test\Quantfabric.Test.Android\obj
 rmdir /s /q test\Quantfabric.Test.iOS\bin
@@ -23,5 +42,7 @@ rmdir /s /q build\Material.NuGet\lib\MonoAndroid60
 rmdir /s /q build\Material.NuGet\lib\Xamarin.iOS10
 
 rmdir /s /q .vs
+
+rmdir /s /q packages
 
 pause
--- cleanExtra.bat
+++
@@ -1,5 +0,0 @@
-rmdir /s /q packages
-rm Quantfabric.sln.DotSettings.user
-rm Quantfabric.userprefs
-
-Call clean.bat
